9893616a33ca0daa4cdf9d108c78a0f79f89f14c
Davi Luiz <127554027+Davi-DevRoom@users.noreply.github.com>
2025-05-12 17:16:47 -0400

    Initial commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './CSS-modules/Header.module.css';
+import { Link } from 'react-router-dom';
+import Dog from '../Assets/dogs.svg?react';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+        <nav className={`${styles.nav} container`}>
+            <Link className={styles.logo} to="/" aria-label="Dogs - Home">
+              <Dog />
+            </Link>
+            <Link className={styles.login} to="/Login">
+                Login / Criar
+            </Link>
+        </nav>
+    </header>
+  );
+};
+
+export default Header
