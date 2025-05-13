942a545ac6250a451c38983212eec66e6993c6b0
Davi Luiz <127554027+Davi-DevRoom@users.noreply.github.com>
2025-05-13 15:22:44 -0400

    criação da area de login
    Criação e estilização da area de login do site

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,17 +2,28 @@ import React from 'react';
 import styles from './CSS-modules/Header.module.css';
 import { Link } from 'react-router-dom';
 import Dog from '../Assets/dogs.svg?react';
+import { UserContext } from '../UserContext';
 
 const Header = () => {
+  const { data, userLogout } = React.useContext(UserContext);
+
   return (
     <header className={styles.header}>
         <nav className={`${styles.nav} container`}>
             <Link className={styles.logo} to="/" aria-label="Dogs - Home">
               <Dog />
             </Link>
-            <Link className={styles.login} to="/Login">
-                Login / Criar
-            </Link>
+            {data ? (
+              <Link className={styles.login} to="/conta">
+                  {data.nome}
+                  <button onClick={userLogout}>Sair</button>
+              </Link>
+              ) : (
+              <Link className={styles.login} to="/Login">
+                  Login / Criar
+              </Link>
+            )}
+            
         </nav>
     </header>
   );
